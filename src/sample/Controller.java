6143328ea0de47bbaf418210c316d33791d9a131
andriusbaltrunas <andriusbaltrunas@yahoo.com>
2017-06-15 19:23:58 +0300

    Sample sayHi

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private Label text;
+
+    @FXML
+    private TextField input;
+
+    public void sayHi(ActionEvent event){
+        //String defaultValue = text.getText();
+        String defaultValue = input.getText();
+        if(defaultValue!=null && !defaultValue.equals("")) {
+            text.setText("Labas " + defaultValue);
+        }else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Blogas vartotojo vardas");
+            alert.show();
+        }
+    }
 }
